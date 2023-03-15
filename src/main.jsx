6aee93e6accea55535a072204526b60e2a4caa60
Suarez Matias <be.creativedesing@gmail.com>
2023-03-15 13:51:49 -0300

    .

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
 import { initFirebase} from './firebase/config'
+import ReactDOM from 'react-dom/client'
+import React from 'react'
+import App from './App'
 import './index.css'
 
 initFirebase()
